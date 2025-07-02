f82881696808253e0debd23db1df79e07a2af761
kim-mijin <mycrimosn@naver.com>
2025-07-02 16:52:45 +0900

    장바구니 완성하기

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import React, { useState } from 'react';
 import ProductList from './components/ProductList';
 import Cart from './components/Cart';
 
+
+        // TODO: 카트에 상품을 추가하는 함수를 완성하세요. ✅
+        // 상품은 배열객체로 이루어져 있습니다.
+        // 카트에 상품이 8개 이상이면 alert를 띄워
+        // 더 이상 상품을 추가할 수 없다고 알려주세요. ✅
+
 const App = () => {
     const [cart, setCart] = useState([]);
 
     const addToCart = (product) => {
-        // TODO: 카트에 상품을 추가하는 함수를 완성하세요.
-        // 상품은 배열객체로 이루어져 있습니다.
-        // 카트에 상품이 8개 이상이면 alert를 띄워
-        // 더 이상 상품을 추가할 수 없다고 알려주세요.
-    };
-
+        if (cart.length > 8) {
+    return alert("더이상 상품을 추가할 수 없습니다!")
+    }
+    setCart([...cart, product]);
+};
     return (
         <>
             <div className="container">
